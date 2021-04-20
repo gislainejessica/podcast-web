af7349cbe38388c76665b6f8b7461b19ccddc6c9
gislainejessica <gislainehjessica@gmail.com>
2021-04-20 19:34:18 -0300

    static side generator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
 // import { Header } from '../components/Header'
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1> oi </h1>
+    <>
+      <h1> oi </h1>
+      <p> {JSON.stringify(props.episodes)}</p>
+    </>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:4333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
